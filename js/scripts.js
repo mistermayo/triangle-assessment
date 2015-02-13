eac1433143810b8faa0cdf9952370c05b15a7863
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-13 12:09:03 -0800

    finalize second spec and logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-var triangle = function(length1, length2, length3) {
-  var numbers = [];
+var triangle = function(numbers) {
   var length1 = numbers[0];
-  var length2 = numbers[0];
-  var length3 = numbers[0];
+  var length2 = numbers[1];
+  var length3 = numbers[2];
 
   var triangle = [];
   // var equilateral = {
@@ -15,9 +14,16 @@ var triangle = function(length1, length2, length3) {
   //   length1: [0], length2: [0], length3: [0]
   // }
 
-  if (length1 === length2 ||  length1 === length3) {
-        triangle.push("equilateral").toString();
-        return triangle
-
-  };
+  if (length1 === length2 && length2 === length3) {
+        //  triangle.push[("equilateral").toString();
+        // //  return triangle.[("equilateral")];
+          triangle = "equilateral"
+debugger;
+    } else if ((length1 === length2 && length2 !== length) ||
+      (length1 === length3 && length2 !== length3) || (length3 === length2 && length2 !== length1)) {
+        //  triangle.push("isosceles").toString();
+          // return triangle.[("isosceles")];
+          triangle = "isosceles"
+    };
+      return triangle;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('triangle', function() {
   it("returns equilateral if triangle object is equilateral", function() {
-    expect(triangle([2,2,2])).to.eql(["equilateral"]);
+    expect(triangle([2,2,2])).to.eql("equilateral");
+  });
+
+  it("returns isosceles if triangle object is isosceles", function() {
+    expect(triangle([2,3,2])).to.eql("isosceles");
   });
 });
