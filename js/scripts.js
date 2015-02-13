039f004041912ed9ed8d2d2288a9521d6748195c
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-13 10:23:11 -0800

    add first spec and logic to spec and scripts files

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-var triangle = function(length) {
+var triangle = function(length1, length2, length3) {
+  var numbers = [];
+  var length1 = numbers[0];
+  var length2 = numbers[0];
+  var length3 = numbers[0];
 
-}
+  var triangle = [];
+  // var equilateral = {
+  //   length1: numbers[0], length2: numbers[0], length3: numbers[0]
+  // }
+  // var isosceles = {
+  //   length1: [0], length2: [0], length3: [0]
+  // }
+  // var scalene = {
+  //   length1: [0], length2: [0], length3: [0]
+  // }
+
+  if (length1 === length2 ||  length1 === length3) {
+        triangle.push("equilateral").toString();
+        return triangle
+
+  };
+};
