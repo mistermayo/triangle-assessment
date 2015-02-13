ef495c1145d10555911e132c2d9dea8c117938d1
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-13 14:18:41 -0800

    add final touches to project

--- js/scripts.js
+++ js/scripts.js
@@ -22,9 +22,11 @@ var triangle = function(length1, length2, length3) {
 
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
+
     var length1 = parseInt($("input#length1").val());
     var length2 = parseInt($("input#length2").val());
     var length3 = parseInt($("input#length3").val());
+    
     var result = triangle(length1, length2, length3);
 
     $(".result").text(result);
