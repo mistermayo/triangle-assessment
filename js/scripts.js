d27e0a7168064ee465ea48a0d2fe8b775adcb9dd
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-13 12:14:55 -0800

    create logic and third spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,20 @@ var triangle = function(numbers) {
   var length1 = numbers[0];
   var length2 = numbers[1];
   var length3 = numbers[2];
-
   var triangle = [];
-  // var equilateral = {
-  //   length1: numbers[0], length2: numbers[0], length3: numbers[0]
-  // }
-  // var isosceles = {
-  //   length1: [0], length2: [0], length3: [0]
-  // }
-  // var scalene = {
-  //   length1: [0], length2: [0], length3: [0]
-  // }
 
-  if (length1 === length2 && length2 === length3) {
-        //  triangle.push[("equilateral").toString();
-        // //  return triangle.[("equilateral")];
+    if (length1 === length2 && length2 === length3) {
+
           triangle = "equilateral"
-debugger;
-    } else if ((length1 === length2 && length2 !== length) ||
+
+    } else if ((length1 === length2 && length2 !== length3) ||
       (length1 === length3 && length2 !== length3) || (length3 === length2 && length2 !== length1)) {
-        //  triangle.push("isosceles").toString();
-          // return triangle.[("isosceles")];
+
           triangle = "isosceles"
+
+    } else if ((length1 != length2 && length2 !== length3 && length1 != length3)) {
+
+          triangle = "scalene"
     };
-      return triangle;
+    return triangle;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('triangle', function() {
   it("returns isosceles if triangle object is isosceles", function() {
     expect(triangle([2,3,2])).to.eql("isosceles");
   });
+
+  it("returns scalene if triangle object is scalene", function() {
+    expect(triangle([1,3,2])).to.eql("scalene");
+  });
 });
